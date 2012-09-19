b4837f9b734957901014a828a5b49bb37105e273
Juha Syrjälä <juha.syrjala@gmail.com>
2012-09-19 08:56:57 +0300

    Validation happens now in correct phase.

--- src-cljs/ruuvi_ui/views/create_tracker.cljs
+++ src-cljs/ruuvi_ui/views/create_tracker.cljs
@@ -2,6 +2,7 @@
   (:require [enfocus.core :as ef]
             [ruuvi-ui.api :as api]
             [ruuvi-ui.view :as view]
+            [ruuvi-ui.views.navigation :as navi]
             )
   (:require-macros [enfocus.macros :as em])
   (:use [ruuvi-ui.log :only [debug info warn error]
@@ -15,16 +16,19 @@
         code (.val (js/$ "#tracker-code"))
         shared-secret (.val (js/$ "#tracker-secret"))
         demo-password (.val (js/$ "#access-password"))]
-    (api/create-tracker {:name name
-                         :code code
-                         :shared-secret shared-secret
-                         })
-  (.preventDefault event)
-  (.stopPropagation event)
-  ))
+    (.preventDefault event)
+    (.stopPropagation event)
+    (if (= demo-password (str "#" (str "ruuvi") "penkki"))
+           (api/create-tracker {:name name
+                                :code code
+                                :shared-secret shared-secret
+                                :success-fn #(navi/display-page :trackers)
+                                })
+           (info "Password is not correct.")
+    )))
 
 (em/defaction init-components []
-  [:#create-tracker-button] (em/listen :click create-tracker))
+  [:#create-tracker-form] (em/listen :submit create-tracker))
 
 (defmethod view/init-content :create-tracker []
   (init-components))
